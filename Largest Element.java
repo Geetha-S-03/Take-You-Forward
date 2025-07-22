8e0b87fc3bd165b8f3134201b3c3b9e4a5ffb233
Geetha-S-03 <168105598+Geetha-S-03@users.noreply.github.com>
2025-07-22 17:27:04 +0530

    Create Largest Element.java

---
+++ Largest Element.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+class Solution {
+    public int largestElement(int[] nums) {
+       int max=nums[0];
+       for(int i=0;i<nums.length;i++){
+        if(nums[i]>max){
+            max=nums[i];
+            }
+       }
+       return max;
+    }
+}
